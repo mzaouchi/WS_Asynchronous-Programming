bcb30685e707e033858165a9591d2d0c49da9c5c
mzaouchi <mzaouchi@gmail.com>
2024-08-21 09:43:37 +0100

    Checkpoint

---
+++ Checkpoint.js
@@ -0,0 +1,26 @@
+// Exercice 1
+// var t = [1,2,3,9,8]
+
+// const attente=()=> new Promise((resolve)=> setTimeout(resolve,1000))
+
+// const Affiche=async(tab)=>{
+//     for(let i = 0; i <tab.length;i++){
+//             console.log(tab[i])
+//             await attente()
+//     }
+// }
+
+// Affiche(t)
+
+
+// Exercice 2
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then(response => response.json())
+// .then(json => console.log(json))
+
+
+// Exercice 3
+// fetch('https://jsaceholder.typicode.com/users')
+// .then(response => response.json())
+// .then(json => console.log(json))
+// .catch((error)=>console.log("3andi mochkla fel api el fouleni"))
